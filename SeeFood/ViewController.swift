f142085bd059fedb375b9764a6c1cf0e9d5a2305
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-05-13 15:31:27 +1000

    Adding hotdog conditional

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -51,12 +51,21 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
                 fatalError("Model failed to process image")
             }
             print(results)
+            
+            if let firstResult = results.first {
+                if firstResult.identifier.contains("hotdog") {
+                    self.navigationItem.title = "Hotdog!"
+                } else {
+                    self.navigationItem.title = "Not Hotdog!"
+                }
+            }
+            
         }
         
         let handler = VNImageRequestHandler(ciImage: image)
         
         do {
-            try! handler.perform([request])
+            try handler.perform([request])
         }
         catch {
             print(error)
