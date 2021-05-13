6ebbe272f6c1e56699f50f3a097fedff7ea670f7
Dylan Park <64296934+dylpark@users.noreply.github.com>
2021-05-13 13:44:24 +1000

    Getting image recognition results

--- SeeFood/ViewController.swift
+++ SeeFood/ViewController.swift
@@ -28,10 +28,41 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         
         if let userPickedImage = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
             imageView.image = userPickedImage
+            
+            guard let ciImage = CIImage(image: userPickedImage) else {
+                fatalError("Could not convert UIImage to CIImage")
+            }
+            
+            detect(image: ciImage)
+            
         }
         
         imagePicker.dismiss(animated: true, completion: nil)
     
+    }
+    
+    func detect(image: CIImage) {
+        guard let model = try? VNCoreMLModel(for: Inceptionv3().model) else {
+            fatalError("Loading coreML model failed")
+        }
+        
+        let request = VNCoreMLRequest(model: model) { request, error in
+            guard let results = request.results as? [VNClassificationObservation] else {
+                fatalError("Model failed to process image")
+            }
+            print(results)
+        }
+        
+        let handler = VNImageRequestHandler(ciImage: image)
+        
+        do {
+            try! handler.perform([request])
+        }
+        catch {
+            print(error)
+        }
+        
+        
     }
 
     @IBAction func cameraTapped(_ sender: Any) {
